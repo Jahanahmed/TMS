01c9dbb53c6a5b4cdd28990d62bf991328c33615
Jahan Ahmed <jahanahmed395@gmail.com>
2017-07-29 21:16:37 +0600

    Project Created

---
+++ TMS/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TMS.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            return View();
+        }
+
+        public JsonResult Info()
+            {
+            //if (id == null) return Json(false, JsonRequestBehavior.DenyGet);
+
+            var student = new Student();
+            student.ID = "1";
+            student.Name = "Jamal";
+
+            var jsonItem = new { student.Name };
+
+            return Json(jsonItem, JsonRequestBehavior.AllowGet);
+    }
+    }
+
+    public class Student
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TMS/Scripts/TMS/StudentCreate.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+
+
+
+});
+
+var Student = [
+    { Id: 1, Name: "Kamal" },
+    { Id: 2, Name: "Jamal" }];
+
+$(document.body).on("click", "#savebtn", function () {
+
+    //$.ajax({
+
+    //    //type
+    //   // url
+    //   // contentType
+    //  //  data
+    //  //  success
+
+    //    type: "GET", 
+    //    url: "/Students/Info",
+    //    contentType: "application/json; charset=utf-8",
+    //    data: JSON.stringify(),
+    //    success: function (data) {
+    //        alert(data.Name);
+    //    }
+    //});
+
+    var url = "/Students/Info";
+    $.get(url, function(responseData){
+        alert(responseData);
+    })
+
+});
